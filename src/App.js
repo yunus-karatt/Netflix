08e7f87b0120b003568b216b845088d60d3ef647
yunus <yyas404@gmail.com>
2023-11-29 23:22:47 +0530

    tmdb api integrate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import NavBar from "./components/NavBar/NavBar";
 import './App.css'
 import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
+import {originals,action} from './urls'
 
 function App() {
+
+
   return (
     <div className="App">
      <NavBar/>
      <Banner/>
+     <RowPost url={originals} title="Netflix Originals"/>
+     <RowPost url={action} title="Action" isSmall />
     </div>
   );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,21 +1,38 @@
 import "./Banner.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../../axios";
+import { API_KEY, imageUrl } from "../../constants/constants";
 
 function Banner() {
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`/trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        const randomIndex = Math.floor(
+          Math.random() * response.data.results.length
+        );
+        setMovie(response.data.results[randomIndex]);
+      });
+  }, []);
   return (
-    <div className="banner">
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})`,
+      }}
+    >
       <div className="content">
-        <h1 className="title">movie name</h1>
+        <h1 className="title">
+          {movie ? (movie.title ? movie.title : movie.name) : ""}
+        </h1>
         <div className="banner-buttons">
           <button className="btn">Play</button>
           <button className="btn">My list</button>
         </div>
-        <h1 className="description">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab dolor
-          dolore debitis vitae fugiat tempora ducimus aliquid exercitationem
-          dolores nemo cupiditate voluptatibus ut, temporibus eveniet
-          perferendis neque possimus qui? Eaque.
-        </h1>
+
+        <h1 className="description">{movie ? movie.overview : ""}</h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
